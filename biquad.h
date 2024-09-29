bda7b90af2cb823fb61c9c8a9fd4c3a71c4819a4
Xavier_laptop <xbs20@hotmail.com>
2024-09-28 21:02:40 -0600

    filtro funciona

--- biquad.h
+++ biquad.h
@@ -34,12 +34,14 @@ public:
     *out = *in*b0_+z1_;
     z1_=*in*b1_-a1_**out+z2_;
     z2_=*in*b2_-a2_**out;
+    return true;
 };
 
     inline sample_t processSample(sample_t input){
         sample_t output = input*b0_+z1_;
         z1_=input*b1_-a1_*output+z2_;
         z2_=input*b2_-a2_*output;
+        return output;
 };   
 
     
