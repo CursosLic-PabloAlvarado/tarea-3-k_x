2f80f459c788810d73eb004affbe72ecabf1ff76
keylorRG <keylor.rivera.12@gmail.com>
2024-10-04 11:42:07 -0600

    Hay salida de audio con AVX, falta definir los estados intermedios

--- biquad.cpp
+++ biquad.cpp
@@ -1,10 +1,11 @@
-
 #include "biquad.h"
-#include <xsimd/xsimd.hpp>
+#include <immintrin.h>  // Para AVX
 #include <iostream>
+#include <cmath>
 using sample_t = jack::client::sample_t;
+
 // Constructor por defecto
-biquad::biquad() : b0_(0.0), b1_(0.0), b2_(0.0), a1_(0.0), a2_(0.0), z1_(0.0), z2_(0.0), set_mode_('p'){
+biquad::biquad() : b0_(0.0), b1_(0.0), b2_(0.0), a1_(0.0), a2_(0.0), z1_(0.0), z2_(0.0), set_mode_('p') {
 }
 
 // Destructor
@@ -18,24 +19,22 @@ void biquad::setCoefficients(const std::vector<sample_t>& coeffs) {
     b2_ = coeffs[2];
     a1_ = coeffs[4];
     a2_ = coeffs[5];
-    a_= -a1_*b1_+a1_*a1_*b1_+b2_-a2_*b1_;
-    b_= -a1_*a1_*b1_-a1_*a1_*a1_*b1_-a1_*b2_+a1_*a2_*b1_-a2_*b1_+a2_*a1_*b1_;
-    c_=-a1_*a1_*a1_+a1_*a2_+a2_*a1_;
-    // Precargar los coeficientes en los registros AVX
-    b0_avx = _mm256_set1_ps(b0_);
-    b1_avx = _mm256_set1_ps(b1_);
-    b2_avx = _mm256_set1_ps(b2_);
-    a1_avx = _mm256_set1_ps(a1_);
-    a2_avx = _mm256_set1_ps(a2_);
-    // Precargar a_, b_, y c_ en registros AVX
-    a_avx = _mm256_set1_ps(a_);
-    b_avx = _mm256_set1_ps(b_);
-    c_avx = _mm256_set1_ps(c_);
+    a_ = -a1_*b0_ + b1_;
+    b_ = a1_*a1_ *b0_-a1_*b1_-a2_*b0_+b2_; //x[n] en y[n+2]
+    c_ = -a1_*b0_+b1_;          //x[n+1] en y[n+2]
+    d_ = a1_*a1_-a2_;    //z1 en y[n+2]
+
+    e_ = -a1_*a1_*a1_*b0_+a1_*a1_*b1_+2*a1_*a2_*b0_-a1_*b2_-a2_*b1_;   //x[n] en y[n+3]
+    f_ = a1_*a1_*b0_-a1_*b1_-a2_*b0_+b2_; //x[n+1]
+    h_ = -a1_*b0_+b1_;   //x[n+2]
+    i_ = -a1_*a1_*a1_+2*a1_*a2_; //z1
+    j_ = a1_*a1_-a2_; // z2
+
     // Pre-cargar la matriz A en AVX: 4 filas para representar las operaciones sobre 4 muestras
-    A_avx[0] = _mm256_set_ps(b0_, b0_, b0_, b0_, 0.0f, 0.0f, 0.0f, 0.0f);  // Fila 1 para b0
-    A_avx[1] = _mm256_set_ps(b1_, b1_, b1_, b1_, z1_, z2_, 0.0f, 0.0f);  // Fila 2 para b1 y estados
-    A_avx[2] = _mm256_set_ps(b2_, b2_, b2_, b2_, 0.0f, 0.0f, z1_, z2_);  // Fila 3 para b2 y estados
-    A_avx[3] = _mm256_set_ps(-a1_, -a1_, -a1_, -a1_, 0.0f, 0.0f, z1_, z2_);  // Fila 4 para a1
+    A_row_1 = _mm256_set_ps(b0_, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f);
+    A_row_2 = _mm256_set_ps(a_, b0_, 0.0f, 0.0f, -a1_, 1.0f, 0.0f, 0.0f);
+    A_row_3 = _mm256_set_ps(b_, c_, b0_, 0.0f, d_, -a1_, 0.0f, 0.0f);
+    A_row_4 = _mm256_set_ps(e_, f_, h_, b0_, i_, j_, 0.0f, 0.0f);
 
     // Inicializamos los estados del filtro en 0 al principio
     z1_ = 0.0f;
@@ -52,26 +51,41 @@ bool biquad::process(jack_nframes_t nframes, const sample_t* in, sample_t* out) 
 
     while (i + simd_size <= nframes) {
         // Cargar las 4 entradas en el vector X (junto con z1_ y z2_)
-      __m256 X_avx = _mm256_set_ps(0.0f, 0.0f, z2_, z1_, in[i+3], in[i+2], in[i+1], in[i]);
+        X_avx = _mm256_set_ps(in[i], in[i+2], in[i+3],in[i+4],z1_,z2_,0.0f,0.0f);
 
-        // Calcular las salidas y[n], y[n+1], y[n+2], y[n+3] como producto de A * X
-        __m256 y_avx = _mm256_add_ps(
-            _mm256_mul_ps(A_avx[0], X_avx),  // b0_*x[n], b0_*x[n+1] ...
-            _mm256_add_ps(
-                _mm256_mul_ps(A_avx[1], X_avx),  // b1_*x[n], b1_*x[n+1] ...
-                _mm256_add_ps(
-                    _mm256_mul_ps(A_avx[2], X_avx),  // b2_*x[n]...
-                    _mm256_mul_ps(A_avx[3], X_avx)   // -a1_*y[n]...
-                )
-            )
-        );
-
-        // Guardar las salidas en el buffer
-        _mm256_storeu_ps(&out[i], y_avx);
+        // Multiplicar la primera fila de A por X
+        __m256 result1 = _mm256_mul_ps(A_row_1, X_avx);
+        __m256 sum1 = _mm256_hadd_ps(result1, result1);
+        sum1 = _mm256_hadd_ps(sum1, sum1);
+        float sum1_high = _mm_cvtss_f32(_mm256_extractf128_ps(sum1, 1));
+        float sum1_low = _mm_cvtss_f32(_mm256_castps256_ps128(sum1));
+        out[i] = sum1_high + sum1_low;
+        //
+        __m256 result2 = _mm256_mul_ps(A_row_2, X_avx);
+        __m256 sum2 = _mm256_hadd_ps(result2, result2);
+        sum2 = _mm256_hadd_ps(sum1, sum1);
+        float sum2_high = _mm_cvtss_f32(_mm256_extractf128_ps(sum2, 1));
+        float sum2_low = _mm_cvtss_f32(_mm256_castps256_ps128(sum2));
+        out[i+1] = sum2_high + sum2_low;
+        //
+        __m256 result3 = _mm256_mul_ps(A_row_3, X_avx);
+        __m256 sum3 = _mm256_hadd_ps(result3, result3);
+        sum3 = _mm256_hadd_ps(sum3, sum3);
+        float sum3_high = _mm_cvtss_f32(_mm256_extractf128_ps(sum3, 1));
+        float sum3_low = _mm_cvtss_f32(_mm256_castps256_ps128(sum3));
+        out[i+2] = sum3_high + sum3_low;
+        //
+        __m256 result4 = _mm256_mul_ps(A_row_4, X_avx);
+        __m256 sum4 = _mm256_hadd_ps(result4, result4);
+        sum4 = _mm256_hadd_ps(sum4, sum4);
+        float sum4_high = _mm_cvtss_f32(_mm256_extractf128_ps(sum4, 1));
+        float sum4_low = _mm_cvtss_f32(_mm256_castps256_ps128(sum4));
+        out[i+3] = sum4_high + sum4_low;
+        
 
         // Actualizar los estados intermedios (z1_, z2_)
         z1_ = in[i+3];  // último valor de la entrada
-        z2_ = out[i+3];  // último valor de la salida
+        z2_ = out[i];   // último valor de la salida
 
         i += simd_size;  // de 4 en 4
     }
@@ -85,6 +99,7 @@ bool biquad::process(jack_nframes_t nframes, const sample_t* in, sample_t* out) 
 
     return true;
 }
+
 
 
 // Método para procesar una sola muestra
--- biquad.h
+++ biquad.h
@@ -11,11 +11,14 @@ class biquad : public jack::client {
 public:
     // Coeficientes del filtro
     sample_t b0_, b1_, b2_, a1_, a2_; //parametros del filtro
-    sample_t a_, b_, c_; //parametros para la implementación optimizada
+    sample_t a_, b_, c_,d_,e_,f_, h_, i_, j_; //parametros para la implementación optimizada
     sample_t z1_, z2_;
     __m256 b0_avx, b1_avx, b2_avx, a1_avx, a2_avx;
     __m256 a_avx, b_avx, c_avx;  // Nuevos coeficientes precargados en AVX
-    __m256 A_avx[4];
+    __m256 A_row_1;
+    __m256 A_row_2;
+    __m256 A_row_3;
+    __m256 A_row_4;
     // Variables de estado
     char set_mode_;
     
